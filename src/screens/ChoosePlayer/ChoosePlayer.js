3028eda4eea145c9bb8ac116472c20cb30d18584
Noy <60noypearl@gmail.com>
2017-07-24 17:54:03 +0300

    code cleanup Chooseplayer screen

--- src/screens/ChoosePlayer/ChoosePlayer.js
+++ src/screens/ChoosePlayer/ChoosePlayer.js
@@ -1,19 +1,11 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react'
-import AutoComplete from 'material-ui/AutoComplete'
-import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import { withRouter } from 'react-router-dom'
-
-import { heroesNames } from '../../utils/heroes'
+import UsernameField from './components/UsernameField'
+import HeroAutocompleteField from './components/HeroAutocompleteField'
 import styles from './styles.css'
 
-const heroes = heroesNames.map(hero => hero[0] + hero.substring(1, hero.length).toLowerCase())
-
-
-const textFieldStyle = {
-  color: '#fff',
-}
 
 class ChoosePlayer extends Component {
   constructor(props) {
@@ -24,7 +16,6 @@ class ChoosePlayer extends Component {
     }
   }
 
-
   onNextButtonClick = () => {
     if (this.state.name && this.state.hero) {
       // router.push('/chat')
@@ -34,29 +25,18 @@ class ChoosePlayer extends Component {
   handleHeroInputUpdate = (hero) => {
     this.setState({ hero })
   }
+
+  handleUsernameTextChange = (val) => {
+    this.setState({ name: val })
+  }
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.centeredBox}>
           <div className={styles.title} >Choose your hero</div>
-          <AutoComplete
-            hintText="Type your hero"
-            dataSource={heroes}
-            onNewRequest={this.handleHeroInputUpdate}
-            filter={AutoComplete.caseInsensitiveFilter}
-            style={{ marginRight: 5 }}
-            textColor="#fff"
-            hintColor="#fff"
-            inputStyle={textFieldStyle}
-            hintStyle={textFieldStyle}
-          />
-          <TextField
-            hintText="enter your name"
-            hintStyle={{ color: '#EEEEEE' }}
-            inputStyle={textFieldStyle}
-            onChange={e => this.setState({ name: e.target.value,
-            })}
-          />
+          <HeroAutocompleteField onHeroChoose={hero => this.handleHeroInputUpdate(hero)} />
+          <UsernameField onTextChange={(e, val) => this.handleUsernameTextChange(val)} />
+
           <div>
             <RaisedButton
               label="next"
@@ -66,6 +46,7 @@ class ChoosePlayer extends Component {
               style={{ marginTop: '5%' }}
             />
           </div>
+
         </div>
       </div>
     )
---
+++ src/screens/ChoosePlayer/components/HeroAutocompleteField/HeroAutocompleteField.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import AutoComplete from 'material-ui/AutoComplete'
+import PropTypes from 'prop-types'
+import { heroesNames } from '../../../../utils/heroes'
+
+const heroes = heroesNames.map(hero => hero[0] + hero.substring(1, hero.length).toLowerCase())
+
+const textFieldStyle = {
+  color: '#fff',
+}
+
+const HeroAutocompleteField = ({ onHeroChoose }) => (
+  <AutoComplete
+    hintText="Type your hero"
+    dataSource={heroes}
+    onNewRequest={onHeroChoose}
+    filter={AutoComplete.caseInsensitiveFilter}
+    style={{ marginRight: 5 }}
+    textColor="#fff"
+    hintColor="#fff"
+    inputStyle={textFieldStyle}
+    hintStyle={textFieldStyle}
+  />
+    )
+
+HeroAutocompleteField.propTypes = {
+  onHeroChoose: PropTypes.func.isRequired,
+}
+
+export default HeroAutocompleteField
